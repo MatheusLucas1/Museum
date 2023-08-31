350a09e133859f2ffb85092835b846cfba700f7d
Matheus <Matheus.lucas1105@gmail.com>
2023-08-31 15:19:56 -0300

    javadoc comments adicionados

--- src/main/java/com/betrybe/museumfinder/dto/MuseumCreationDto.java
+++ src/main/java/com/betrybe/museumfinder/dto/MuseumCreationDto.java
@@ -2,6 +2,9 @@ package com.betrybe.museumfinder.dto;
 
 import com.betrybe.museumfinder.model.Coordinate;
 
+/**
+ * Museum creation DTO class.
+ */
 public record MuseumCreationDto(String name, String description, String address,
                                 String collectionType, String subject, String url,
                                 Coordinate coordinate) {
