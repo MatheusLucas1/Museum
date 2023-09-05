a4ff213276feef1da6d8e6878fa3bfeb1010600e
Matheus <Matheus.lucas1105@gmail.com>
2023-09-05 09:57:43 -0300

    POST route for /museums

---
+++ src/main/java/com/betrybe/museumfinder/controller/MuseumController.java
@@ -0,0 +1,42 @@
+package com.betrybe.museumfinder.controller;
+
+
+import com.betrybe.museumfinder.dto.MuseumDto;
+import com.betrybe.museumfinder.model.Coordinate;
+import com.betrybe.museumfinder.model.Museum;
+import com.betrybe.museumfinder.service.MuseumServiceInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.betrybe.museumfinder.util.ModelDtoConverter;
+/**
+ * MuseumController class.
+ */
+@RestController
+@RequestMapping("/museums")
+public class MuseumController {
+  MuseumServiceInterface service;
+
+  @Autowired
+  public MuseumController(MuseumServiceInterface service) {
+    this.service = service;
+  }
+
+  /**
+   * PostMapping.
+   */
+
+  @PostMapping
+  public ResponseEntity<Museum> createMuseum(@RequestBody MuseumDto museumDto) {
+    Museum museum = ModelDtoConverter.dtoToModel(museumDto);
+    Museum CreatedMuseum = service.createMuseum(museum);
+    return ResponseEntity.status(HttpStatus.CREATED).body(CreatedMuseum);
+  }
+
+
+
+}
